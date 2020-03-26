07e1e0e3952913e8d41842fb2eef6fd01afea530
Jiajun Yan <jiajun.yan@outlook.com>
2020-03-26 14:25:04 +0800

    添加 jumbotron

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,58 @@
+import React from 'react';
 import Radium from 'radium';
-import { GlobalStyles } from '../styles';
+import { GlobalStyles, ResetStyles, theme, layout } from '../styles';
+import Header from '../components/Header'
+import HeroBackground from '../assets/images/hero.jpg';
 
-const Index = () => {
-	return (
-		<div>
-			<GlobalStyles />
-		</div>
-	);
+const { colors, fontWeight } = theme;
+
+const jumbotron = {
+	self: {
+		width: '100%',
+		height: 400,
+		backgroundColor: colors.gray,
+		backgroundImage: `url(${HeroBackground})`,
+		backgroundSize: 'cover',
+		display: 'flex',
+		position: 'relative' as 'relative'
+	},
+	cover: {
+		position: 'absolute' as 'absolute',
+		width: '100%',
+		height: '100%',
+		backgroundColor: 'rgba(0,0,0,0.1)',
+		zIndex: 0
+	},
+	titleWrapper: {
+		position: 'relative' as 'relative',
+		alignSelf: 'flex-end',
+		zIndex: 1,
+		...layout.contentSize.desktop,
+		marginBottom: 100
+	},
+	title: {
+		fontSize: 80,
+		color: colors.white,
+		fontWeight: fontWeight.ultra,
+	}
 };
+
+class Index extends React.Component {
+	render() {
+		return (
+			<React.Fragment>
+				<ResetStyles />
+				<GlobalStyles />
+				<Header></Header>
+				<div style={jumbotron.self}>
+					<div style={jumbotron.cover}></div>
+					<div style={jumbotron.titleWrapper}>
+						<h1 style={jumbotron.title}>Jiajun Yan</h1>
+					</div>
+				</div>
+			</React.Fragment>
+		);
+	}
+}
 
 export default Radium(Index);
